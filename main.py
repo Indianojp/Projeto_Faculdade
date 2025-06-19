38d265948ee6409df8c354fdfc2c6492011a90cf
Indianojp <jp.asilva657@gmail.com>
2025-06-19 00:38:09 -0300

    beta

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from gitwil import app
+from waitress import serve
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
+    return ['Servidor WSGI acessível pela internet via Waitress!']
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8000)
